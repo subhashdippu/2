38705d250994cb2a85016e3d7ce5ef366bd2dd4a
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-08-26 13:36:01 +0530

    config db

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
