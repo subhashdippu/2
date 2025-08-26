b3fcf13c0c91ed2c0da7a47facb374cc6fe8d401
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-08-26 13:44:44 +0530

    adding lead model

---
+++ Backend/models/Lead.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+
+const leadSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+
+    phone: {
+      type: Number,
+      required: true,
+    },
+    altPhone: {
+      type: Number,
+      default: "",
+    },
+    email: { type: String, required: true },
+
+    status: {
+      type: String,
+      enum: ["New", "In Progress", "Converted", "Qualified", "Follow-Up"],
+      default: "New",
+    },
+    qualification: {
+      type: String,
+      enum: ["High School", "Bachelor's", "Master's", "Other"],
+      default: "",
+    },
+    interestField: {
+      type: String,
+      enum: ["Web Development", "Data Science", "UI/UX Design", "Other"],
+      default: "",
+    },
+    source: {
+      type: String,
+      enum: ["Website", "Social Media", "Email Campaign", "Cold Call", "Other"],
+      default: "",
+    },
+    assignedTo: {
+      type: String,
+      default: "",
+    },
+    jobInterest: {
+      type: String,
+      default: "",
+    },
+    state: {
+      type: String,
+      default: "",
+    },
+    city: {
+      type: String,
+      default: "",
+    },
+    passoutYear: {
+      type: String,
+      default: "",
+    },
+    heardFrom: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true, // automatically adds createdAt and updatedAt
+  }
+);
+
+module.exports = mongoose.model("Lead", leadSchema);
