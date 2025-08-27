46091ef98416bd01e8c53598a4ea587b822fed10
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-08-27 12:31:28 +0530

    adding middleware

---
+++ Backend/middleware/errorHandler.js
@@ -0,0 +1,9 @@
+const errorHandler = (err, req, res, next) => {
+  const status = res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(status).json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? "....." : err.stack,
+  });
+};
+
+module.exports = errorHandler;
